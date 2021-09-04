585e3b7fa7e60fdc06e17075bf9b44c6bd4c82f8
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-04 16:46:17 -0300

    fix: removing some line codes of the App file

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ShowProducts from './screens/ShowProducts';
 import InformationProduct from './screens/InformationProduct';
+import ReadBarCode from './screens/ReadBarCode';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,10 +11,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="ShowProducts"
-          component={ShowProducts}
-        />
+        <Stack.Screen name="ReadBarCode" component={ReadBarCode} />
+        <Stack.Screen name="ShowProducts" component={ShowProducts} />
         <Stack.Screen name="InformationProduct" component={InformationProduct} />
       </Stack.Navigator>
     </NavigationContainer>
