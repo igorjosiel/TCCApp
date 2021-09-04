0a5ee4eadd2d5733f2c79432edc436fbb3539cea
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-04 19:34:38 -0300

    feat: adding one variable to typing the bar code or name of the product

--- screens/ReadBarCode.tsx
+++ screens/ReadBarCode.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { TextInput, StyleSheet, Button, Text } from 'react-native';
 import * as Styled from './components';
 
 const messageOne = "Olá! Desfrute do nosso app. Um aplicativo desenvolvido para você buscar as informações dos produtos que precisa de forma fácil e eficiente."
 const messageTwo = "Digite o código de barras ou o nome do produto no campo abaixo ou capture o mesmo com a câmera do seu celular.";
 
 const ReadBarCode = ({navigation}) => {
+  const [search, setSearch] = useState('');
+
   return <Styled.ScreenShowCards>
     <Styled.CardHomeScreen disabled={false} height="170px" width="80%">
         <Styled.TextHomeScreen>{messageOne}</Styled.TextHomeScreen>
@@ -14,17 +15,17 @@ const ReadBarCode = ({navigation}) => {
         <Styled.TextHomeScreen>{messageTwo}</Styled.TextHomeScreen>
     </Styled.CardHomeScreen>
     <Styled.Input
-        // onChangeText={onChangeNumber}
-        // value=
+        onChangeText={(text) => setSearch(text)}
+        value={search}
         placeholder="Digite aqui..."
       />
-    <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => console.log("Foi")}>
+    <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => console.log("Search: ", search)}>
         <Styled.TextButton>
             <Styled.IconButton name="search" size={15} color="black" />
             Pesquisar
         </Styled.TextButton>
     </Styled.Button>
-    <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => console.log("Foi")}>
+    <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => console.log("Search: ", search)}>
         <Styled.TextButton>
             <Styled.IconButton name="camera" size={15} color="black" />
             Ligar a câmera
