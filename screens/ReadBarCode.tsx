c5ceb98a2d68828d28abafacd29eaec9ab47a2a0
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-04 16:44:49 -0300

    feat: adding a new table to text the bar code or capture the code through the camera

---
+++ screens/ReadBarCode.tsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import { View, Button, Text } from 'react-native';
+import * as Styled from './components';
+import formatMoney from '../utils/formatMoney';
+
+const ReadBarCode = ({navigation}) => {
+
+  return <Styled.StyledScreenShowCards>
+    
+  </Styled.StyledScreenShowCards>
+}
+
+
+export default ReadBarCode;
