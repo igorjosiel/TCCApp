d4b8ff01a491bf43343717396561d3c799892340
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-18 15:21:33 -0300

    feat: initializing the way to show the data.

--- screens/ShowProducts.tsx
+++ screens/ShowProducts.tsx
@@ -1,37 +1,45 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View } from '../components/Themed';
 import * as Styled from './components';
 import {getProducts} from '../services';
 
-export default function ShowProducts({ navigation }) {
-  const products = [{
+export default function ShowProducts({ navigation, route }) {
+  const products = [];
+
+useEffect(() => {
+  const productsBAckEnd = [{
     name:"arroz",
     price: "30",
     description:"arroz Tio João",
     category: "comida",
     barcode:"47575757585"
-},{
-  name:"chocolate",
-  price: "30",
-  description:"Nestle",
-  category: "comida",
-  barcode:"47575757585"
-},{
-  name:"bolo",
-  price: "30",
-  description:"Tia Nastácia",
-  category: "comida",
-  barcode:"47575757585"
-}];
+  },{
+    name:"chocolate",
+    price: "30",
+    description:"Nestle",
+    category: "comida",
+    barcode:"47575757585"
+  },{
+    name:"bolo",
+    price: "30",
+    description:"Tia Nastácia",
+    category: "comida",
+    barcode:"47575757585"
+  }];
 
-useEffect(() => {
-  // console.log(getProducts());
+  productsBAckEnd && productsBAckEnd.map((product, index) => {
+    if(product.name === route.params) {
+      console.log("route.params: ", route.params, product);
+      products.push(product);
+      console.log("products: ", products);
+    }
+  });
 }, []);
 
   return (
     <Styled.ScreenShowCards>
-      {
-    products && products.map((product, index) => {
+      {console.log('Products: ', typeof products)}
+    {products && products.length > 0 && products.map((product, index) => {
       return (<Styled.Card key={index} height={170} width="92%" onPress={() => navigation.navigate('InformationProduct', product)}>
         <Styled.Title center={true} height="20">{product.name}</Styled.Title>
         <View>
