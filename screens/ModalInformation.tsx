9f579137ee29513a80f4dfcf15e44173da4d4991
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-18 09:56:57 -0300

    fix: removing the modal to build a new screen to show that the user must inform a value.

--- screens/ModalInformation.tsx
+++ screens/ModalInformation.tsx
@@ -3,40 +3,9 @@ import { Alert, Modal, StyleSheet, Text, TouchableHighlight, View } from 'react-
 import Icon from 'react-native-vector-icons/FontAwesome';
 import * as Styled from './components';
 
-export default function ModalInformation(props) {
-    const { openModal, closeModal, message } = props;
+export default function ModalInformation() {
 
     return (
-      <View style={{marginBottom: 'auto', marginTop: 'auto'}}>
-        <Modal
-          animationType="fade"
-          transparent={true}
-          visible={openModal}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{height: 300, backgroundColor: 'white', width: "80%", marginTop: 'auto', marginBottom: 'auto',marginLeft: 'auto', marginRight: 'auto', borderRadius: 10}}>
-            <View>
-              <View style={{height: '115%', alignItems: 'center'}}>
-                <Icon name="exclamation" size={75} style={{marginBottom: 'auto', marginTop: 'auto'}} color="#000" />
-              </View>
-              <View style={{height: '65%', alignItems: 'center'}}>
-              <Text style={{textAlign: 'center', height: '100%', width: "90%", marginLeft: 'auto', marginRight: 'auto', fontWeight: 'bold'}}>{message}</Text>
-              </View>
-              <View style={{height: '45%', alignItems: 'center', width: '100%'}}>
-                <TouchableHighlight
-                  style={{width: "50%", backgroundColor: '#00BFFF', height: '50%', marginRight: 'auto', marginLeft: 'auto', borderRadius: 5}}
-                  onPress={() => {
-                    closeModal()
-                  }}>
-                  <Styled.TextButton>
-                    Voltar
-                  </Styled.TextButton>
-                </TouchableHighlight>
-              </View>
-            </View>
-          </View>
-        </Modal>
-      </View>
+      
     );
   }
--- screens/ReadBarCode.tsx
+++ screens/ReadBarCode.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import * as Styled from './components';
-import ModalInformation from './ModalInformation';
 
 const messageOne = "Olá! Desfrute do nosso app. Um aplicativo desenvolvido para você buscar as informações dos produtos que precisa de forma fácil e eficiente."
 const messageTwo = "Digite o código de barras ou o nome do produto no campo abaixo ou capture o mesmo com a câmera do seu celular.";
@@ -8,11 +7,9 @@ const messageModal = "Por favor, digite o código de barras ou o nome do produto
 
 const ReadBarCode = ({navigation}) => {
   const [search, setSearch] = useState('');
-  const [modalVisible, setModalVisible] = useState(false);
 
   const searchBarCode = () => {
     if(search.length > 0) navigation.navigate('ShowProducts', search);
-    else setModalVisible(true);
   }
 
   const onpenCamera = () => {
@@ -20,40 +17,30 @@ const ReadBarCode = ({navigation}) => {
   }
 
   return <Styled.ScreenShowCards>
-    {console.log("Foi: ", modalVisible)}
-    {modalVisible ? <ModalInformation
-      openModal={modalVisible}
-      closeModal={() => setModalVisible(!modalVisible)}
-      message={messageModal}
-    /> : null}
-    {!modalVisible ? 
     <Styled.CardHomeScreen disabled={false} height={170} width="80%">
         <Styled.TextHomeScreen>{messageOne}</Styled.TextHomeScreen>
-    </Styled.CardHomeScreen> : null}
-    {!modalVisible ?
+    </Styled.CardHomeScreen>
     <Styled.CardHomeScreen height={130} width="80%">
         <Styled.TextHomeScreen>{messageTwo}</Styled.TextHomeScreen>
-    </Styled.CardHomeScreen> : null}
-    {!modalVisible ? <Styled.Input
+    </Styled.CardHomeScreen>
+    <Styled.Input
         onChangeText={(text) => setSearch(text)}
         value={search}
         maxLength={!isNaN(search) ? 13 : 25}
         placeholder="Digite aqui..."
-      /> : null}
-      {!modalVisible ?
+      />
       <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => searchBarCode()}>
         <Styled.TextButton>
             <Styled.IconButton name="search" size={15} color="black" />
             Pesquisar
         </Styled.TextButton>
-    </Styled.Button> : null}
-    {!modalVisible ?
+    </Styled.Button>
     <Styled.Button title="Pesquisar" color="#00BFFF" onPress={() => onpenCamera()}>
         <Styled.TextButton>
             <Styled.IconButton name="camera" size={15} color="black" />
             Ligar a câmera
         </Styled.TextButton>
-    </Styled.Button> : null}
+    </Styled.Button>
   </Styled.ScreenShowCards>
 }
 
