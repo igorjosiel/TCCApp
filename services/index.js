d2a1d47608b5cd3acea013d01bbb7ce222f28c18
Igor Josiel <igorjosielvitalino@gmail.com>
2021-11-10 00:08:32 -0300

    feat: searching for the registers that the user wants to see the informations.

--- services/index.js
+++ services/index.js
@@ -3,30 +3,11 @@ import { http } from './config';
 const textUrl = "/products";
 
 export const listProducts = async (filter) => {
-    if (filter === undefined) {
-        const { data } = await http.get('/products');
-        
-        return {
-            data: data.data,
-            message: data.message,
-            pagination: data.pagination
-        }
-    }
-    else {
-        const { name, cpf, ra } = filter;
-
-        return http.get(`${textUrl}/?name=${name}&cpf=${cpf}&ra=${ra}`);
+    const { data } = await http.get(`/products?filter=${filter}`);
+    
+    return {
+        data: data.data,
+        message: data.message,
+        pagination: data.pagination
     }
 }
-
-// export const createStudent = (params) => {
-//     return http.post(`${textUrl}`, params);
-// }
-
-// export const deleteStudent = (id) => {
-//     return http.delete(`${textUrl}/${id}`);
-// }
-
-// export const updateStudent = (id, params) => {
-//     return http.put(`${textUrl}/${id}`, params);
-// }
